13afd4b5788512c1db72cb73a5194e3d61093340
Martin Molzer <ga65guy@mytum.de>
2025-10-03 22:42:01 +0200

    move loader script to its own file
    change async handling to only happen in loaders, not dependencies

---
+++ crates/wasm_split_cli/src/split_wasm.js
@@ -0,0 +1,46 @@
+import { initSync } from "./main.js";
+let sharedImports = undefined;
+function getSharedImports() {
+    if (sharedImports === undefined) {
+        const mainExports = initSync(undefined, undefined);
+        sharedImports = {
+            __wasm_split: {
+                __indirect_function_table: mainExports.__indirect_function_table,
+                __stack_pointer: mainExports.__stack_pointer,
+                memory: mainExports.memory,
+            },
+        };
+    }
+    return sharedImports;
+}
+function wrapAsyncCb(callee) {
+    return async (callbackIndex, callbackData) => {
+        let success;
+        try {
+            await callee();
+            success = true;
+        } catch (e) {
+            console.error(e);
+            success = false;
+        } finally {
+            const sharedImports = getSharedImports();
+            sharedImports.__wasm_split.__indirect_function_table.get(callbackIndex)(callbackData, success);
+        }
+    }
+}
+function makeLoad(url, deps) {
+    const loader = async () => {
+        const parallelStuff = deps.map(d => d());
+        const fetchSelf = fetch(url);
+        parallelStuff.push(fetchSelf);
+        await Promise.all(parallelStuff);
+        const response = await fetchSelf;
+        const imports = getSharedImports();
+        return await WebAssembly.instantiateStreaming(response, imports);
+    };
+    let loadingModule = undefined;
+    return () => {
+        if (loadingModule === undefined) loadingModule = loader();
+        return loadingModule;
+    }
+}
