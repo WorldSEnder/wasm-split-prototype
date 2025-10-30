3283d4967f3c36520e0c2f81464f3d3d2d85854f
Martin Molzer <WorldSEnder@users.noreply.github.com>
2025-10-30 18:53:37 +0100

    run in virtual x environment

--- integration/runner.sh
+++ integration/runner.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 THIS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd -- "$THIS_DIR"/../test-runner || exit 1
 unset RUSTUP_TOOLCHAIN CARGO # make sure the below invocation is done with the toolchain in test-runner
