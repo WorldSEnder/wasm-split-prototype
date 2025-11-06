c44e264a214fc58b615e81f6b8f078d668f0f03c
Martin Molzer <WorldSEnder@users.noreply.github.com>
2025-11-06 21:51:27 +0100

    add size reporting to test runner

--- integration/runner.sh
+++ integration/runner.sh
@@ -2,4 +2,7 @@
 THIS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd -- "$THIS_DIR"/../test-runner || exit 1
 unset RUSTUP_TOOLCHAIN CARGO # make sure the below invocation is done with the toolchain in test-runner
-cargo run -p wasm-split-test-runner -- "$@"
+export XCARGO_BIN_NAME="$CARGO_BIN_NAME"
+export XCARGO_MANIFEST_DIR="$CARGO_MANIFEST_DIR"
+export XCARGO_TARGET_TMPDIR="$CARGO_TARGET_TMPDIR"
+cargo run --release -p wasm-split-test-runner -- "$@"
