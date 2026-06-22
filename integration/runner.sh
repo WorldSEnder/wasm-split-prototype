cd5c1209e709afe8f2c13975c2ad98ee7c5acc5c
WorldSEnder <WorldSEnder@users.noreply.github.com>
2026-06-22 13:30:20 +0200

    chore: improve test debugging (#39)
    * store runner temp dir in target directory
    
    no longer lives in global tempdir/XDG_TEMPDIR
    this lets us find the generates files more quickly when running
    with NO_HEADLESS=1
    
    * print report to stable file
    
    * add ci cache & bump checkout

--- integration/runner.sh
+++ integration/runner.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 THIS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd -- "$THIS_DIR"/../test-runner || exit 1
-unset RUSTUP_TOOLCHAIN CARGO # make sure the below invocation is done with the toolchain in test-runner
+export XRUSTUP_TOOLCHAIN="$RUSTUP_TOOLCHAIN"
 export XCARGO_BIN_NAME="$CARGO_BIN_NAME"
 export XCARGO_MANIFEST_DIR="$CARGO_MANIFEST_DIR"
 export XCARGO_TARGET_TMPDIR="$CARGO_TARGET_TMPDIR"
+export XCARGO_PKG_NAME="$CARGO_PKG_NAME"
+unset RUSTUP_TOOLCHAIN CARGO # make sure the below invocation is done with the toolchain in test-runner
 cargo run --release -p wasm-split-test-runner -- "$@"
